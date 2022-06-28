bea38d00bcc61a023778be16474502ddb24e5766
sam-levin <sam.ky.levin@gmail.com>
2022-06-27 22:17:15 -0700

    creating graphql mutations and functionality

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savebook(PUT STUFF HERE!!!)
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type removeBook {
+    ACCEPTS bookID AS PARAMETER, RETURNS [USER]
+  }
+
+  type User {
+    _id: String!
+    username: String!
+    email: String!
+    bookCount: int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String! - THIS WILL BE RETURNED FROM GOOGLE BOOKS API
+    authors: [String!]
+    description: String!
+    title: String!
+    image: 
+    link:
+  }
+
+`
+
+module.exports = typeDefs;
