a31b455399d52c1aa78b0714d444761bd5c562e9
sam-levin <sam.ky.levin@gmail.com>
2022-07-08 19:38:52 -0700

    updating for help

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,40 +7,35 @@ type Query {
     me: User
     users: [User]
     user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    savebook(PUT STUFF HERE!!!)
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
   }
 
-  type removeBook {
-    ACCEPTS bookID AS PARAMETER, RETURNS [USER]
+  type Auth {
+     token: ID!
+     user: User
   }
+
 
   type User {
     _id: String!
     username: String!
     email: String!
-    bookCount: int
+    bookCount: Int
     savedBooks: [Book]
   }
 
   type Book {
-    bookId: String! - THIS WILL BE RETURNED FROM GOOGLE BOOKS API
+    bookId: String! 
     authors: [String!]
     description: String!
     title: String!
-    image: 
-    link:
-  }
 
-  
+  }
 `
+  
 
 module.exports = typeDefs;
